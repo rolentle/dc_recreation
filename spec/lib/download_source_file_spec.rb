503349cb35fa7d0306f070b0e44d3dcfd9897ffb
Rolen Le <rolentle@gmail.com>
2017-04-19 09:54:23 -0400

    Add test to check for content-type of file

--- spec/lib/download_source_file_spec.rb
+++ spec/lib/download_source_file_spec.rb
@@ -5,11 +5,17 @@ describe DownloadSourceFile do
     expect(DownloadSourceFile).to be_truthy
   end
 
-  describe 'pulls down the files and parse' do
+  describe 'pulls down the files' do
     let (:results) { DownloadSourceFile.execute }
     it 'returns 200' do
       VCR.use_cassette('get csv file') do
         expect(results.status).to be 200
+      end
+    end
+
+    it 'returns a csv' do
+      VCR.use_cassette('get csv file') do
+        expect(results.headers['content-type']).to eq "text/csv; charset=utf-8"
       end
     end
   end
