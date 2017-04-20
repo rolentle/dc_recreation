7c47a2a5e933fa5607c5684e08786caa935d8a51
Rolen Le <rolentle@gmail.com>
2017-04-20 19:34:33 -0400

    Parse the body of response via CSV

--- lib/download_source_file.rb
+++ lib/download_source_file.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class DownloadSourceFile
   attr_reader :response
 
@@ -5,6 +7,12 @@ class DownloadSourceFile
   end
 
   def initialize
-    @response = Faraday.get('https://opendata.arcgis.com/datasets/7122c1c815314588abe5c1864da8a355_3.csv')
+    @response ||= Faraday.get('https://opendata.arcgis.com/datasets/7122c1c815314588abe5c1864da8a355_3.csv')
+  end
+
+  def parsed_results
+    @parsed_results ||= CSV.parse(response.body.force_encoding('UTF-8'), headers: true, header_converters: :symbol).map do |row|
+      row.to_hash
+    end
   end
 end
--- spec/lib/download_source_file_spec.rb
+++ spec/lib/download_source_file_spec.rb
@@ -18,5 +18,25 @@ describe DownloadSourceFile do
         expect(results.headers['content-type']).to eq "text/csv; charset=utf-8"
       end
     end
+
+    let (:parsed_results) { DownloadSourceFile.new.parsed_results }
+    it 'can parse and return arrary of results' do
+      VCR.use_cassette('get csv file') do
+        expect(parsed_results.length).to be > 0
+      end
+    end
+
+    let (:expected_keys) do
+      %w(
+      x y objectid name address use_type ward_id school_site  pool pool_name
+      child_care senior_services web_url phone status xcoord ycoord addrid fitness_center
+      ).map(&:to_sym)
+    end
+
+    it 'object has keys' do
+      VCR.use_cassette('get csv file') do
+        expect(parsed_results.first.keys).to eq expected_keys
+      end
+    end
   end
 end
