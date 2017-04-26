467ee9680269334dc1bacfac251d401d9a2222d0
Rolen Le <rolentle@gmail.com>
2017-04-26 08:43:22 -0400

    Refactor RecreationCenter#fitness_center?

--- app/models/recreation_center.rb
+++ app/models/recreation_center.rb
@@ -2,4 +2,7 @@ class RecreationCenter < ApplicationRecord
   def pool?
     ![nil, 'none'].include?(pool)
   end
+  def fitness_center?
+    ![nil, 'No'].include?(fitness_center)
+  end
 end
--- spec/models/recreation_center_spec.rb
+++ spec/models/recreation_center_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe RecreationCenter, :type => :model do
-  let(:recreation_center) { RecreationCenter.new(pool: pool) }
   describe '#pool?' do
+  let(:recreation_center) { RecreationCenter.new(pool: pool) }
     describe 'when #pool == nil' do
       let(:pool) { nil }
       it 'returns false' do
@@ -21,6 +21,30 @@ RSpec.describe RecreationCenter, :type => :model do
       let(:pool) { 'indoor' }
       it 'returns false' do
         expect(recreation_center.pool?).to be true
+      end
+    end
+  end
+
+  describe '#fitness_center?' do
+  let(:recreation_center) { RecreationCenter.new(fitness_center: fitness_center) }
+    describe 'when #fitness_center == nil' do
+      let(:fitness_center) { nil }
+      it 'returns false' do
+        expect(recreation_center.fitness_center?).to be false
+      end
+    end
+
+    describe 'when #pool == No' do
+      let(:fitness_center) { 'No' }
+      it 'returns false' do
+        expect(recreation_center.fitness_center?).to be false
+      end
+    end
+
+    describe 'when #pool == Cardio Room' do
+      let(:fitness_center) { 'Cardio Room' }
+      it 'returns false' do
+        expect(recreation_center.fitness_center?).to be true
       end
     end
   end
